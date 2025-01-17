395bf1a7a12c83b8d115581c77a8f1b985fe0b64
MOHAMED-ECH <echarfaouy.mohamed@student.emi.ac.ma>
2025-01-17 03:49:40 +0100

    home page RAS

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from app.blueprints.home import home
+import os
 
-app = Flask(__name__)
-app.secret_key = 'votre_clé_secrète_ici'  # Nécessaire pour la session
+app = Flask(__name__, 
+    static_folder='app/static',  # Spécifiez le dossier static
+    template_folder='app/templates'  # Spécifiez le dossier templates
+)
 
-# Enregistrement du blueprint
+app.secret_key = 'votre_clé_secrète_ici'
 app.register_blueprint(home)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
